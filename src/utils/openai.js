8dd1342e11a50ef5d235730e6c4248c8890ed6de
MaxPayne8 <maxpayne0852.com>
2023-12-11 23:18:32 +0530

    commit_final

--- src/utils/openai.js
+++ src/utils/openai.js
@@ -2,7 +2,7 @@ import OpenAI from "openai";
 import { API_GPT_KEY } from "./constants";
 
 const openai = new OpenAI({
-  apiKey: API_GPT_KEY,
+  apiKey: "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",
   dangerouslyAllowBrowser: true, // defaults to process.env["OPENAI_API_KEY"]
 });
 
